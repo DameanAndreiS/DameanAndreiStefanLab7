d3e1d4f787d73e3264c7b63ee4788ca6102c0e0a
Dămean Andrei-Ștefan <117767484+Damean-Andrei-Stefan@users.noreply.github.com>
2024-12-02 15:21:10 +0200

    Lab9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,12 +1,14 @@
 using DameanAndreiStefanLab7.Models;
+
 namespace DameanAndreiStefanLab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -14,10 +16,34 @@ public partial class ListPage : ContentPage
         await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        var selectedProduct = (Product)listView.SelectedItem;
+        if (selectedProduct != null)
+        {
+            await App.Database.DeleteProductAsync(selectedProduct);
+            var shopl = (ShopList)BindingContext;
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+        }
+        else
+        {
+            await DisplayAlert("Error", "Please select a product to delete.", "OK");
+        }
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
     }
 }
